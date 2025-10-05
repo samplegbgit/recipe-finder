f884aa2804273236dd576f040e29ab2c2f74540c
samplegbgit <gaurikbose022@gmail.com>
2025-10-05 10:37:46 +0530

    Final styling and deployment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import About from "./pages/About";
+//import About from "./pages/About";
 import RecipeDetail from "./pages/RecipeDetail";
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
       <div className="container mt-4">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          {/* <Route path="/about" element={<About />} /> */}
           <Route path="/recipe/:id" element={<RecipeDetail />} />
         </Routes>
       </div>
